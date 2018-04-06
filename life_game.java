10fd92e27f5f6c8fa24f7c7c2c8ba31191994802
pabloftorres <pablo.fernandez@mataro.epiaedu.cat>
2018-04-06 16:10:45 +0200

    Edit

--- life_game.java
+++ life_game.java
@@ -1,12 +1,33 @@
 package proyectos;
 
-import java.util.Scanner;
-
 public class life_game {
 
 	public static void main(String[] args) {
 		
-		int table[][] = new int[12][12];
+		cLector lector = new cLector();
 		
+		int tableA[][] = new int[12][12];
+		int tableB[][] = new int[12][12];
+		int coordenadaA = 0;
+		int coordenadaB = 0;
+		int next = 0;
+		int i;
+		int j;
+		
+		while (coordenadaA != -1 && coordenadaB != -1) {
+			coordenadaA = lector.llegirEnter();
+			tableA[next][next] = coordenadaA;
+			coordenadaB = lector.llegirEnter();
+			tableA[next][next] = coordenadaB;
+			System.out.println(" " + " * ");
+			next++;
+		}
+		//Recorrido tablero
+		for (i = 1; i < next; i++) {
+			for (j = 1; j < next; j++) {
+				
+			}
+		}
 	}
-}
+
+ }
