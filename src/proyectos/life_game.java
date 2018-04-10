bbe442522f4328077acf075b5bfedad802828449
alexvoc <37587130+alexvoc@users.noreply.github.com>
2018-04-10 16:52:39 +0200

    LIFE GAME V2

--- src/proyectos/life_game.java
+++ src/proyectos/life_game.java
@@ -1,60 +1,64 @@
-package proyectos;
+package Activitats_metodes;
+
+import java.util.Scanner;
 
 public class life_game {
 
 	public static void main(String[] args) {
-		
 		cLector lector = new cLector();
 		
 		int tableA[][] = new int[12][12];
 		int tableB[][] = new int[12][12];
-		int coordenadaA = 0;
-		int coordenadaB = 0;
-		int next = 0;
-		int i;
-		int j;
-		
-		while (coordenadaA != -1 && coordenadaB != -1) {
-			coordenadaA = lector.llegirEnter();
-			tableA[next][next] = coordenadaA;
-			coordenadaB = lector.llegirEnter();
-			tableA[next][next] = coordenadaB;
-			System.out.println(" " + " * ");
-			next++;
-		}
-		//Recorrido tablero
-		for (i = 1; i < next; i++) {
-			for (j = 1; j < next; j++) {
-				
+ 		int fila = 0, columna = 0;
+ 		int next = 0;
+ 		int i;
+ 		int j;
+ 		
+ 		fila = CoordenadaA();
+ 		columna = CoordenadaB();
+ 		
+	 		while (fila != -1 && columna != -1) {
+	 				
+	 			tableA[next][next] = fila;
+	 			tableA[next][next] = columna;
+	 			System.out.println(" " + " * ");
+	 			next++;
+	 			System.out.println(" * " + " ");
+	 			next++;
+	 		}
+ 		
+			for (i = 1; i < next; i++) {
+				for (j = 1; j < next; j++) {
+				}
 			}
-		}
 	}
-	public static int cleanA() {
-		int tablaA[][] = new int[12][12];
-		int next = 0;
-		int i = 0;
-		int j = 0;
+	public int ImpTaula() {
+		int guion;
+		int aster;
 		
-		for (i = 0; i < next;) {
-			
-			for (j = 0; j < next;) {
-				tablaA[i][j] = 0;
-			}
-		}
-		return tablaA[i][j];
+		
 	}
-	public static int cleanB() {
-		int tablaB[][] = new int[12][12];
-		int next = 0;
-		int i = 0;
-		int j = 0;
+	public static int CoordenadaA() {
+		cLector lector = new cLector();
+		
+		int vFila;
+		
+		System.out.print("Insereix 1ra coordenada: ");
+		vFila = lector.llegirEnter();
+		
+		return vFila;
+		
+	}
+	public static int CoordenadaB() {
+		cLector lector = new cLector();
+		
+		int vColumna;
+		
+		System.out.print("Insereix 2na coordenada: ");
+		vColumna = lector.llegirEnter();
+		
+		return vColumna;
+		
+	}
+}
 
-		for (i = 0; i < next;) {
-			
-			for (j = 0; j < next;) {
-				tablaB[i][j] = 0;
-			}
-		}
-		return tablaB[i][j];
-	}
- }
