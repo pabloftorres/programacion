46ae94f7870ad777477e42bdf92582687358b827
pabloftorres <37587642+pabloftorres@users.noreply.github.com>
2018-04-10 17:50:36 +0200

    LIFE GAME V3

--- src/proyectos/life_game.java
+++ src/proyectos/life_game.java
@@ -1,4 +1,4 @@
-package Activitats_metodes;
+package proyectos;
 
 import java.util.Scanner;
 
@@ -6,59 +6,65 @@ public class life_game {
 
 	public static void main(String[] args) {
 		cLector lector = new cLector();
-		
+
 		int tableA[][] = new int[12][12];
 		int tableB[][] = new int[12][12];
- 		int fila = 0, columna = 0;
- 		int next = 0;
- 		int i;
- 		int j;
- 		
- 		fila = CoordenadaA();
- 		columna = CoordenadaB();
- 		
-	 		while (fila != -1 && columna != -1) {
-	 				
-	 			tableA[next][next] = fila;
-	 			tableA[next][next] = columna;
-	 			System.out.println(" " + " * ");
-	 			next++;
-	 			System.out.println(" * " + " ");
-	 			next++;
-	 		}
- 		
-			for (i = 1; i < next; i++) {
-				for (j = 1; j < next; j++) {
-				}
-			}
-	}
-	public int ImpTaula() {
-		int guion;
-		int aster;
+		int fila = 0, columna = 0;
+		int next = 0;
 		
+		tablero();
 		
-	}
+		fila = CoordenadaA();
+		columna = CoordenadaB();
+
+		while (fila != -1 && columna != -1) {
+
+			tableA[fila][columna] = fila;
+			tableA[fila][columna] = columna;
+			System.out.println("1");
+			next++;
+		}
+		clean(tableA);
+		clean(tableB);
+	}		
 	public static int CoordenadaA() {
 		cLector lector = new cLector();
-		
+
 		int vFila;
-		
+
 		System.out.print("Insereix 1ra coordenada: ");
 		vFila = lector.llegirEnter();
-		
+
 		return vFila;
-		
 	}
 	public static int CoordenadaB() {
 		cLector lector = new cLector();
-		
+
 		int vColumna;
-		
+
 		System.out.print("Insereix 2na coordenada: ");
 		vColumna = lector.llegirEnter();
-		
+
 		return vColumna;
-		
+	}
+	public static void tablero() {
+		int raya;
+		for (raya = 0; raya <= 10; raya++) {
+			System.out.println("-");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.print("-");
+			System.out.println("|");
+			System.out.println("|");
+		}
+	}
+	public static void clean(int table[][]) {
+		int i, j;
+
+		for (i = 1; i < 12; i++) {
+			for (j = 1; j < 12; j++) {
+				table[i][j] = 0;
+			}
+		}
 	}
 }
-
