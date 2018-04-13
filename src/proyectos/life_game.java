6dcfa7cbc3d8bc7df5fef541221989b3f8db067c
alexvoc <37587130+alexvoc@users.noreply.github.com>
2018-04-13 15:12:40 +0200

    V.6

--- src/proyectos/life_game.java
+++ src/proyectos/life_game.java
@@ -1,70 +1,68 @@
 package proyectos;
 
 public class life_game {
-
-	private static int size= 12;
 	public static void main(String[] args) {
+		final int size = 12;
 		cLector lector = new cLector();
 
 		int tableA[][] = new int[size][size];
 		int tableB[][] = new int[size][size];
 		int fila = 0, columna = 0;
 
-		
-		clean(tableA);
-		clean(tableB);
-
-		fila = CoordenadaA();
-		columna = CoordenadaB();
-
-		while (fila != -1 && columna != -1) {
-			tableA[fila][columna] = 1;
-
-			fila = CoordenadaA();
-			columna = CoordenadaB();
-			
+		do {
+			fila = Coordenada("Insereix 1ra coordenada: ");
+			columna = Coordenada("Insereix 2na coordenada: ");
 		}
-		
-	mostrarGen(tableA);
-	
-	}	
+		while (fila != -1 && columna != -1);
+
+		String continuar = "Si";
+
+		while(continuar.equals("Si")) {
+			omplirTableB(tableA, tableB);
+			igualarTables(tableA, tableB);
+			clean(tableB);
+			mostrarGen(tableA);
+			System.out.println("Vols fornicar mes?");
+			continuar = lector.llegirString();
+		}
+	}
+
+	private static void igualarTables(int tableA[][], int tableB[][]) {
+		for (int c = 1; c <= 11; c++) {
+			for (int c2 = 1; c2 <= 11; c2++) {
+				tableA[c][c2] = tableB[c][c2];
+			}
+		}
+	}
+
 	public static void mostrarGen(int table[][]) {
-		int i;
-		int j;
-		for (i = 1; i < 10; i++) {
-			
-			for (j = 1; j < 10; j++) {
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
 				System.out.print("| " + table[i][j] + " |");
 			}
 			System.out.println(" ");
 		}
  	}
-	public static int CoordenadaA() {
+	public static int Coordenada(String hola) {
 		cLector lector = new cLector();
-
-		int vFila;
-
-		System.out.print("Insereix 1ra coordenada: ");
-		vFila = lector.llegirEnter();
-
-		return vFila;
-	}
-	public static int CoordenadaB() {
-		cLector lector = new cLector();
-
-		int vColumna;
-
-		System.out.print("Insereix 2na coordenada: ");
-		vColumna = lector.llegirEnter();
-
-		return vColumna;
+		System.out.print(hola);
+		return lector.llegirEnter();
 	}
 	public static void clean(int table[][]) {
-		int i, j;
-
-		for (i = 1; i < 10; i++) {
-			for (j = 1; j < 10; j++) {
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
 				table[i][j] = 0;
+			}
+		}
+	}
+
+	public static void omplirTableB(int tableA[][], int tableB[][]) {
+		int vivesTaulerA = 0;
+
+		for (int c = 1; c <= 11; c++) {
+			for (int c2 = 1; c2 <= 11; c2++) {
+				//fer les condicions, i despres comprovar si esta viva o morta per saber que fer
+
 			}
 		}
 	}
