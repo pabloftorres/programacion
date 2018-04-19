28153260a20e5a7bcca6d6473d6fe3d5326b65d8
pabloftorres <pablo.fernandez@mataro.epiaedu.cat>
2018-04-19 15:25:06 +0200

    V2.1

--- src/proyectos/hundir_la_flota.java
+++ src/proyectos/hundir_la_flota.java
@@ -1,7 +1,7 @@
-package proyectos;
+package Activitats_metodes;
 
-public class hundir_la_flota {
-	
+public class Hundir_la_flota {
+
 	private static int tamaño = 6;
 	public static void main(String[] args) {
 		cLector lector = new cLector();
@@ -11,16 +11,29 @@ public class hundir_la_flota {
 		int tableA[][] = new int[tamaño][tamaño];
 		int tableB[][] = new int[tamaño][tamaño];
 		
-		fila = Coordenada("Introdueix una coordenada");
-		columna = Coordenada("Introdueix una coordenada");
 		
-		while (barcos <= 5) {
+		System.out.println("Player1 et toca");
+		
+		while (barcos < 5) {
+			fila = Coordenada("Introdueix una coordenada:");
+			columna = Coordenada("Introdueix una coordenada:");
 			tableA[columna][fila] = 1;
 			barcos++;
-			tablero(tableA);
 		}
-				
+		tablero(tableA);
+		barcos = 0;
+		
+		System.out.println("Player2 et toca");
+		while (barcos < 5) {
+			fila = Coordenada("Introdueix una coordenada:");
+			columna = Coordenada("Introdueix una coordenada:");
+			tableB[columna][fila] = 1;
+			barcos++;
+		}
+		tablero(tableB);
+		barcos = 0;
 	}
+	
 	public static int Coordenada(String frase) {
 		cLector lector = new cLector();
 		
@@ -32,11 +45,11 @@ public class hundir_la_flota {
 	public static void tablero(int table[][]) {
 		for (int i = 1; i <= 5; i++) {
 			
-			for (int j = 1; 1 <= 5; j++) {
+			for (int j = 1; j <= 5; j++) {
 				System.out.print("| " + table[i][j] + " |");
 			}
 			System.out.println(" ");
 		}
 		
-	}
+}
 }
