87da7acd0a6a173d9f28da679e09b409358c46da
alexvoc <37587130+alexvoc@users.noreply.github.com>
2018-04-20 16:11:23 +0200

    V.3

--- src/proyectos/hundir_la_flota.java
+++ src/proyectos/hundir_la_flota.java
@@ -1,4 +1,4 @@
-package proyectos;
+package Activitats_metodes;
 
 public class hundir_la_flota {
 
@@ -6,17 +6,18 @@ public class hundir_la_flota {
 	public static void main(String[] args) {
 		cLector lector = new cLector();
 		int barcos = 0;
-		int fila, columna = 0;
+		int fila = 0, columna = 0;
 		
 		int tableA[][] = new int[tamaño][tamaño];
 		int tableB[][] = new int[tamaño][tamaño];
 		
 		
-		System.out.println("Player1 et toca");
+		System.out.println("Player1 introdueix coordenades per ficar vaixells.");
 	
 		while (barcos < 5) {
 			fila = Coordenada("Introdueix una coordenada:");
 			columna = Coordenada("Introdueix una coordenada:");
+			error(columna, fila);
 			tableA[columna][fila] = 1;
 			barcos++;
 			
@@ -25,21 +26,27 @@ public class hundir_la_flota {
 		tablero(tableA);
 		barcos = 0;
 		
-		System.out.println("Player2 et toca");
+		System.out.println("Player2 introdueix coordenades per ficar vaixells.");
 		while (barcos < 5) {
 			fila = Coordenada("Introdueix una coordenada:");
 			columna = Coordenada("Introdueix una coordenada:");
+			error(columna, fila);
 			tableB[columna][fila] = 1;
 			barcos++;
+			
 		}
+		
 		tablero(tableB);
 		barcos = 0;
+		
 	}
 	
 	public static int Coordenada(String frase) {
 		cLector lector = new cLector();
-		
+		int columna = 0; 
+		int fila = 0;
 		System.out.print(frase);
+		
 				
 		return lector.llegirEnter();
 	}
@@ -55,25 +62,20 @@ public class hundir_la_flota {
 		
 	}
 
-	public static int error(int table [][]) {
-		cLector lector = new cLector();
-		int fila = 0;
-		int columna = 0;
-		if (table[columna][fila] > 5) {
-			System.out.println("Error, introdueix una coordenada que no sigui superior a 5");
-			fila = Coordenada("Introdueix una coordenada:");
-			columna = Coordenada("Introdueix una coordenada:");
+	public static void error(int f, int c) {
+		if ((f > 5) & (c > 5)) {
+			System.out.println("Error, introdueix una coordenada que no sigui superior a 5");	
 		}
-		return lector.llegirEnter();
+		
 	}
 	public static void regles(int tableA[][],int tableB[][]) {
 		String tocatM = null;
 		int aigua;
 		int voltant;
 		
-		for (int f = 1; f <= 10; f++) {
+		for (int f = 1; f <= 5; f++) {
 
-			for (int c = 1; c <= 10; c++) {
+			for (int c = 1; c <= 5; c++) {
 				if (tableA [f][c] == 1) {
 					voltant = tableA[f-1][c] + tableA[f+1][c] + tableA[f][c+1] + tableA[f][c-1] + tableA[f-1][c-1] 
 							+ tableA[f-1][c+1] + tableA[f+1][c+1] + tableA[f+1][c+1];
@@ -87,4 +89,15 @@ public class hundir_la_flota {
 			}
 		}
 	}
+	public static int atac(int table []) {
+		cLector lector = new cLector();
+		int fila = 0;
+		int columna = 0;
+		
+		System.out.println("Comença la fase d'atac. Introdueix 2 coordenades per atacar. Primerament una i després l'altre.");
+		fila = Coordenada("Introdueix una coordenada:");
+		columna = Coordenada("Introdueix una coordenada:");
+		
+		return lector.llegirEnter();	
+		}
 }
