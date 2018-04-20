f695cc10166732fc0b8cb1002631855e26c9fd15
alexvoc <37587130+alexvoc@users.noreply.github.com>
2018-04-20 16:50:35 +0200

    V_4.5
    Metode Error, metode veins arreglat.

--- src/proyectos/hundir_la_flota.java
+++ src/proyectos/hundir_la_flota.java
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-package proyectos;
-
-public class hundir_la_flota {
-
-	private static int size = 6;
-	public static void main(String[] args) {
-		cLector lector = new cLector();
-		int barcos = 0;
-		int fila = 0;
-		int columna = 0;
-		int vecinos;
-
-		int tableA[][] = new int[size][size];
-		int tableB[][] = new int[size][size];
-
-
-		System.out.println("Player1 et toca");
-		while (barcos < 5) {
-			while (fila < 5 || columna < 5) {
-				fila = Coordenada("Introdueix una coordenada:");
-				columna = Coordenada("Introdueix una coordenada:");
-			}
-			
-			vecinos = veins(tableA, fila, columna);
-			if (vecinos == 0) {
-				tableA[columna][fila] = 1;
-				barcos++;
-			}
-		}
-
-		tablero(tableA);
-		barcos = 0;
-
-		System.out.println("Player2 et toca");
-		while (barcos < 5) {
-			fila = Coordenada("Introdueix una coordenada:");
-			columna = Coordenada("Introdueix una coordenada:");
-			vecinos = veins(tableA, fila, columna);
-			if (vecinos == 0) {
-				tableB[columna][fila] = 1;
-				barcos++;
-			}
-		}
-		tablero(tableB);
-		barcos = 0;
-	}
-
-	public static int Coordenada(String frase) {
-		cLector lector = new cLector();
-
-		System.out.print(frase);
-
-		return lector.llegirEnter();
-	}
-
-	public static void tablero(int table[][]) {
-		for (int i = 1; i <= 5; i++) {
-
-			for (int j = 1; j <= 5; j++) {
-				System.out.print("| " + table[i][j] + " |");
-			}
-			System.out.println(" ");
-		}
-	}
-
-	public static int veins (int tableA[][], int f, int c ) {
-		int voltant = 0;
-
-		voltant = tableA[f - 1][c] + tableA[f + 1][c] + tableA[f][c + 1] + tableA[f][c - 1]
-				+ tableA[f - 1][c - 1] + tableA[f - 1][c + 1] + tableA[f + 1][c + 1] + tableA[f + 1][c + 1];
-		
-		return voltant;
-	}
-}
-=======
 package Activitats_metodes;
 
 public class hundir_la_flota {
@@ -93,7 +17,7 @@ public class hundir_la_flota {
 		while (barcos < 5) {
 			fila = Coordenada("Introdueix una coordenada:");
 			columna = Coordenada("Introdueix una coordenada:");
-			error(columna, fila);
+			error(fila,columna);
 			tableA[columna][fila] = 1;
 			barcos++;
 			
@@ -106,7 +30,7 @@ public class hundir_la_flota {
 		while (barcos < 5) {
 			fila = Coordenada("Introdueix una coordenada:");
 			columna = Coordenada("Introdueix una coordenada:");
-			error(columna, fila);
+			error(fila,columna);
 			tableB[columna][fila] = 1;
 			barcos++;
 			
@@ -126,7 +50,6 @@ public class hundir_la_flota {
 				
 		return lector.llegirEnter();
 	}
-	
 	public static void tablero(int table[][]) {
 		for (int i = 1; i <= 5; i++) {
 			
@@ -137,34 +60,21 @@ public class hundir_la_flota {
 		}
 		
 	}
-
-	public static void error(int f, int c) {
-		if ((f > 5) & (c > 5)) {
-			System.out.println("Error, introdueix una coordenada que no sigui superior a 5");	
-		}
-		
-	}
-	public static void regles(int tableA[][],int tableB[][]) {
-		String tocatM = null;
-		int aigua;
-		int voltant;
-		
-		for (int f = 1; f <= 5; f++) {
-
-			for (int c = 1; c <= 5; c++) {
-				if (tableA [f][c] == 1) {
-					voltant = tableA[f-1][c] + tableA[f+1][c] + tableA[f][c+1] + tableA[f][c-1] + tableA[f-1][c-1] 
-							+ tableA[f-1][c+1] + tableA[f+1][c+1] + tableA[f+1][c+1];
-				}
-				if(tableA[f][c] == 0) {
-					System.out.println("Aigua!!");
-				}
-				if(tableA[f][c] == Coordenada(tocatM)) {
-					System.out.println("Tocat i enfonsat!!");
-				}
-			}
+	public static void error(int fila, int columna) {
+		while(fila < 1 || fila > 5 || (columna < 1 || columna > 5)){
+			System.out.println("Error, introdueix una coordenada que no sigui superior a 5");
+			fila = Coordenada("Introdueix una coordenada:");
+			columna = Coordenada("Introdueix una coordenada:");
 		}
 	}
+	public static int veins (int table[][], int f, int c ) {
+		int voltant = 0;
+
+		voltant = table[f - 1][c] + table[f + 1][c] + table[f][c + 1] + table[f][c - 1]
+				+ table[f - 1][c - 1] + table[f - 1][c + 1] + table[f + 1][c + 1] + table[f + 1][c + 1];
+		
+		return voltant;
+}
 	public static int atac(int table []) {
 		cLector lector = new cLector();
 		int fila = 0;
@@ -177,4 +87,3 @@ public class hundir_la_flota {
 		return lector.llegirEnter();	
 		}
 }
->>>>>>> 87da7acd0a6a173d9f28da679e09b409358c46da
