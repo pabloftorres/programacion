e9344f98537b523ba13c7fa4ce784df80902315d
pabloftorres <37587642+pabloftorres@users.noreply.github.com>
2018-04-24 17:13:47 +0200

    V7

--- src/proyectos/hundir_la_flota.java
+++ src/proyectos/hundir_la_flota.java
@@ -1,67 +1,97 @@
-package Activitats_metodes;
+package proyectos;
 
 public class hundir_la_flota {
 
-	private static int tamaño = 6;
+	private static int size = 6;
 	public static void main(String[] args) {
 		cLector lector = new cLector();
 		int barcos = 0;
 		int fila = 0, columna = 0;
-		int atac = 0;
-		
-		int tableA[][] = new int[tamaño][tamaño];
-		int tableB[][] = new int[tamaño][tamaño];
-		
-		
+		int vecinos;
+		int i;
+		int j;
+
+		int tableA[][] = new int[size][size];
+		int tableB[][] = new int[size][size];
+
+
 		System.out.println("Player1 introdueix coordenades per ficar vaixells.");
-	
+
 		while (barcos < 5) {
 			fila = Coordenada("Introdueix una coordenada:");
 			columna = Coordenada("Introdueix una coordenada:");
 			error(fila,columna);
-			tableA[columna][fila] = 1;
-			barcos++;
-			
+			vecinos = veins(tableA, fila, columna);
+			if (vecinos == 0) {
+				tableB[columna][fila] = 1;
+				barcos++;
+			}
 		}
-		
 		tablero(tableA);
 		barcos = 0;
-		
-		
+
 		System.out.println("Player2 introdueix coordenades per ficar vaixells.");
 		while (barcos < 5) {
 			fila = Coordenada("Introdueix una coordenada:");
 			columna = Coordenada("Introdueix una coordenada:");
 			error(fila,columna);
-			tableB[columna][fila] = 1;
-			barcos++;
-			
+			vecinos = veins(tableB, fila, columna);
+			if (vecinos == 0) {
+				tableB[columna][fila] = 1;
+				barcos++;
+			}
 		}
-		
 		tablero(tableB);
-		barcos = 0;
-		atac1vs2(tableA);
-		
+		barcos = 10;
+
+		while (barcos >= 0) {
+			i = 0;
+			j = 0;
+			
+			atac1vs2(tableB);
+			tablero(tableB);
+			for (i = 0; i <= 5; i++) {
+
+				for (j = 0; j <= 5; j++) {
+					if (tableB[i][j] == 1) {
+						barcos--;
+					}
+				}
+			}
+			i = 0;
+			j = 0;
+			
+			atac2vs1(tableA);
+			tablero(tableA);
+			for (i = 0; i <= 5; i++) {
+
+				for (j = 0; j <= 5; j++) {
+					if (tableA[i][j] == 1) {
+						barcos--;
+					}
+				}
+			}
+		}
 	}
-	
+
 	public static int Coordenada(String frase) {
 		cLector lector = new cLector();
 		int columna = 0; 
 		int fila = 0;
 		System.out.print(frase);
-		
-				
+
+
 		return lector.llegirEnter();
 	}
 	public static void tablero(int table[][]) {
 		for (int i = 1; i <= 5; i++) {
-			
+
 			for (int j = 1; j <= 5; j++) {
 				System.out.print("| " + table[i][j] + " |");
 			}
 			System.out.println(" ");
 		}
-		
+
 	}
 	public static void error(int fila, int columna) {
 		while(fila < 1 || fila > 5 || (columna < 1 || columna > 5)){
@@ -75,43 +105,43 @@ public class hundir_la_flota {
 
 		voltant = table[f - 1][c] + table[f + 1][c] + table[f][c + 1] + table[f][c - 1]
 				+ table[f - 1][c - 1] + table[f - 1][c + 1] + table[f + 1][c + 1] + table[f + 1][c + 1];
-		
+
 		return voltant;
-}
-	public static void atac1vs2(int tableB[][]) {
-		int f;
-		int c;
-		
-		System.out.println("Jugador1 et toca atacar.");
-		f = Coordenada("Introdueix una coordenada:");
-		c = Coordenada("Introdueix una coordenada:");
-
-
-			if (tableB[c][f] == 0) {
-				System.out.print("No li has donat a res");
-				}
-				if (tableB[c][f] == 1) {
-				tableB[c][f] = 0;
-				System.out.print("Tocat i enfonsat.");
-			}
-			
 	}
-	public static  void atac2vs1(int tableA [][]) {
+	public static void atac1vs2(int table[][]) {
+		cLector lector = new cLector();
 		int f;
 		int c;
-		
-		System.out.println("Jugador2 et toca atacar.");
+
+		System.out.println("Comença la fase d'atac");
 		f = Coordenada("Introdueix una coordenada:");
 		c = Coordenada("Introdueix una coordenada:");
 
 
-				if (tableA[c][f] == 0) {
-				System.out.print("No li has donat a res");
-				}
-				if (tableA[c][f] == 1) {
-				tableA[c][f] = 0;
-				System.out.print("Tocat i enfonsat.");
-			}
-			
+		if (table[f][c] == 0) {
+			System.out.print("No li has donat a res");
+		}
+		if (table[f][c] == 1) {
+			table[f][c] = 0;
+			System.out.print("Tocat i enfonsat.");
+		}
+	}
+	public static void atac2vs1(int table[][]) {
+		cLector lector = new cLector();
+		int f;
+		int c;
+
+		System.out.println("Comença la fase d'atac.");
+		f = Coordenada("Introdueix una coordenada:");
+		c = Coordenada("Introdueix una coordenada:");
+
+
+		if (table[f][c] == 0) {
+			System.out.print("No li has donat a res");
+		}
+		if (table[f][c] == 1) {
+			table[f][c] = 0;
+			System.out.print("Tocat i enfonsat.");
+		}
 	}
 }
