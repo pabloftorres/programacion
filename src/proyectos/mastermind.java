68c58a7f64f630b74f56782841c625c06dfa379c
alexvoc <37587130+alexvoc@users.noreply.github.com>
2018-05-04 16:38:06 +0200

    Add files via upload
    Player1 , Player2, falta saber si el Player 2 ha esbrinat els colors del Player1.

--- src/proyectos/mastermind.java
+++ src/proyectos/mastermind.java
@@ -9,31 +9,47 @@ package Activitats_metodes;
 			cLector lector = new cLector();
 			cWait wait = new cWait();
 			
-			char color;
-			int bolsa = 0;
+			
 			int number;
 			int tableA[] = new int[size];
 			int tableB[] = new int[size];
-			boolean go = true;
 			
 			System.out.println("-Ara jugarem al joc Mastermind-");
 			System.out.println("-Podras triar entre aquests colors. Blau(B), Negre(N), Vermell(V), Lila(L), Groc(G) i el color Crema(C)-");
-			while (bolsa < 4) {
+			Player1();
+		}
+		public static void Player1() {
+			cLector lector = new cLector();
+			char color;
+			int bolsa = 0;
 				
+				while (bolsa < 4) {
 				System.out.println("Player1, ara es el teu torn.");
 				System.out.println("Escull 4 colors dels anteriors, agefint la lletra esmentada per cada color. Es poden repetir :");
 				color = lector.llegirChar();
-				bolsa++;
+				
+				if ((color =='B')||(color =='N')||(color =='V')||(color =='L')||(color =='G')||(color =='C')) {
+					bolsa++;
+				}
+				else {
+					System.out.println("Introdueix una lletra correcte.");
+
+				}
 			}
 		}
-		
-		public static char color() {
+		public static void Player2() {
 			cLector lector = new cLector();
-			
 			char color;
+			int bolsa = 0;
 			
-			color = lector.llegirChar();		
-			return color;
+				while(bolsa < 4) {
+					System.out.println("Player2, ara es el teu torn.");
+					System.out.println("Haurás d'esbrinar la serie de colors del Player1.");
+					System.out.println("-Podras triar entre aquests colors. Blau(B), Negre(N), Vermell(V), Lila(L), Groc(G) i el color Crema(C)-");
+					System.out.println("Escull 4 colors dels anteriors, agefint la lletra esmentada per cada color. Recorda poden repetir :");
+					
+					color = lector.llegirChar();
+				}
 		}
 		public static void cleanTables(int table[]) {
 			int i;
