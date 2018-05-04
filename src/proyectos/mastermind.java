f5656d8014699dbc2ef6d4986faf3b1b50ac6e2d
alexvoc <37587130+alexvoc@users.noreply.github.com>
2018-05-04 16:07:56 +0200

    Add files via upload
    Player1 4 colors

--- src/proyectos/mastermind.java
+++ src/proyectos/mastermind.java
@@ -1,36 +1,38 @@
-package proyectos;
+package Activitats_metodes;
 
-public class mastermind {
+	public class mastermind {
 
-	private static int size = 4;
+		private static int size = 4;
 
-	public static void main(String[] args) {
-		
-		cLector lector = new cLector();
-		cWait wait = new cWait();
-		
-		char color;
-		int number;
-		int tableA[] = new int[size];
-		int tableB[] = new int[size];
-		boolean go = true;
-		
-		System.out.println("Ara jugarem al joc Mastermind");
-		System.out.println("Hi ha 6 colors. Blau(B), negre(N), vermell(V), lila(L), groc(G) i el color crema(C)");
-		while (go) {
+		public static void main(String[] args) {
 			
-			System.out.println("Player1, ara es el teu torn.");
+			cLector lector = new cLector();
+			cWait wait = new cWait();
 			
+			char color;
+			int bolsa = 0;
+			int number;
+			int tableA[] = new int[size];
+			int tableB[] = new int[size];
+			boolean go = true;
+			
+			System.out.println("-Ara jugarem al joc Mastermind-");
+			System.out.println("-Podras triar entre aquests colors. Blau(B), Negre(N), Vermell(V), Lila(L), Groc(G) i el color Crema(C)-");
+			while (bolsa < 4) {
+				
+				System.out.println("Player1, ara es el teu torn.");
+				System.out.println("Escull 4 colors dels anteriors, agefint la lletra esmentada per cada color. Es poden repetir :");
+				color = lector.llegirChar();
+				bolsa++;
+			}
 		}
-	}
-	
-	public static char color() {
-		cLector lector = new cLector();
 		
-		char color;
-		
-		color = lector.llegirChar();		
-		return color;
-	}
+		public static char color() {
+			cLector lector = new cLector();
+			
+			char color;
+			
+			color = lector.llegirChar();		
+			return color;
+		}
 }
-
