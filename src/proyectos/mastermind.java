234df58d6396d7f8341e8a9807ec0601212ad671
pabloftorres <37587642+pabloftorres@users.noreply.github.com>
2018-05-08 17:08:35 +0200

    V5
    Verified tables added

--- src/proyectos/mastermind.java
+++ src/proyectos/mastermind.java
@@ -1,23 +1,21 @@
-package Activitats_metodes;
+package proyectos;
 
 public class mastermind {
 
-	private static int size = 8;
+	private static int size = 4;
 
 	public static void main(String[] args) {
 
 		int number;
 		char tableA[] = new char[size];
 		char tableB[] = new char[size];
-		int contador = 0;;
+		int contador = 0;
 		
 		System.out.println("Ara jugarem al joc Mastermind");
 		System.out.println("Hi ha 6 colors. Blau(B), negre(N), vermell(V), lila(L), groc(G) i el color crema(C)");
-		while (contador < 4) {
-
-			System.out.println("Player1, ara es el teu torn.");
-			System.out.println("Escolleix quatre colors:");
-			
+		System.out.println("Player1, ara es el teu torn.");
+		System.out.println("Escolleix quatre colors:");
+		while (contador < 4) {			
 			for (number = 0; number < 4; number++) {
 				tableA[number] = choice();
 				contador++;				
@@ -25,13 +23,20 @@ public class mastermind {
 			tablero(tableA);
 		}
 		cleanScreen();
-		System.out.println("Player2, ara es el teu torn.");
-		System.out.println("Endevina els colors del Player1, escollint quatre colors dels anterior:");
-		for (number = 0; number < 4; number++) {
-			tableB[number] = choice();
-			contador++;
+		contador = 0;
+		
+		System.out.println("Player2 ara es el teu torn, tens que endevinar la sequencia de colors");
+		
+		while (verify(tableA, tableB) == false) {
+			for (number = 0; number < 4; number++) {
+				tableB[number] = choice();
+				contador++;
+			}
+			if (verify(tableA, tableB) == false) {
+				System.out.println("La sequencia introduida ha estat erronea");
+			}
+			tablero(tableB);
 		}
-		tablero(tableB);
 	}
 
 	public static char choice() {
@@ -49,6 +54,7 @@ public class mastermind {
 		for (square = 0; square < 4; square++) {
 			System.out.print("[" + table[square] +  "]");
 		}
+		System.out.println("");
 	}
 	
 	public static String cleanScreen() {
@@ -58,7 +64,7 @@ public class mastermind {
 		String clean = " ";
 		int i;
 		
-		wait.wait(2);
+		wait.wait(3);
 		
 		for (i = 0; i <= 10; i++) {
 			System.out.println(clean);
@@ -72,6 +78,24 @@ public class mastermind {
 		
 		for (next = 0; next < 4; next++) {
 			table[next] = 0;
+		}
+	}
+	
+	public static boolean verify(char tableA[], char tableB[]) {
+		
+		int next;
+		int contador = 0;
+		
+		for (next = 0; next < 4; next++) {
+			if (tableA[next] == tableB[next]) {
+				contador++;
+			}
+		}
+		if (contador == 4) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 }
