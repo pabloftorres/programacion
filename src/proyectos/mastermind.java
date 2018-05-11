87e2e6f50ad9f94bfdd364f74595ddfd306e2a99
pabloftorres <37587642+pabloftorres@users.noreply.github.com>
2018-05-11 16:58:00 +0200

    FINAL VERSION

--- src/proyectos/mastermind.java
+++ src/proyectos/mastermind.java
@@ -1,4 +1,4 @@
-package Activitats_metodes;
+package proyectos;
 
 public class mastermind {
 
@@ -6,28 +6,38 @@ public class mastermind {
 
 	public static void main(String[] args) {
 
-		int number;
 		char tableA[] = new char[size];
 		char tableB[] = new char[size];
 		int contador = 1;
-		
+
 		System.out.println("Ara jugarem al joc Mastermind");
 		System.out.println("Hi ha 6 colors. Blau(B), negre(N), vermell(V), lila(L), groc(G) i el color crema(C)");
 		System.out.println("Player1, ara es el teu torn.");
 		System.out.println("Escolleix quatre colors:");
+
 		colors(tableA);
 		cleanScreen();
-		
-		
+
+
 		System.out.println("Player2 ara es el teu torn, tens que endevinar la sequencia de colors");
+
 		colors(tableB);
-		
+
 		while (verify(tableA, tableB) == false && contador < 15) {
+
+			fitxes(tableA, tableB);
+			if (verify(tableA, tableB) == true) {
+				System.out.print("···You win Player2···");
+			}
 			System.out.println("Sequencia de colors introduida incorrecte");
 			colors(tableB);
 			contador++;
-}
+		}
+		if (verify(tableA, tableB) == true) {
+			System.out.print("···You win Player2···");
+		}
 	}
+
 	public static char choice() {
 		cLector lector = new cLector();
 
@@ -36,22 +46,22 @@ public class mastermind {
 		color = lector.llegirChar();		
 		return color;
 	}
+
 	public static void colors(char table[]) {
 		int number;
 		char color;
-				
-			for (number = 0; number < 4; number++) {
-				color = choice();		
 
-				while (color != 'B' && color != 'N' && color != 'L' && color != 'G' && color != 'C' && color != 'V') {
-					System.out.println("Lletra introduida incorrecte");
-					color = choice();
-				}				
-					table[number] = color;
-			}			
-			tablero(table);		
+		for (number = 0; number < 4; number++) {
+			color = choice();		
+
+			while (color != 'B' && color != 'N' && color != 'L' && color != 'G' && color != 'C' && color != 'V') {
+				System.out.println("Lletra introduida incorrecte");
+				color = choice();
+			}				
+			table[number] = color;
+		}			
+		tablero(table);		
 	}
-
 
 	public static void tablero(char table[]) {
 		int square;
@@ -61,36 +71,26 @@ public class mastermind {
 		}
 		System.out.println("");
 	}
-	
+
 	public static String cleanScreen() {
 		cWait wait = new cWait();
-		cLector lector = new cLector();
-		
+
 		String clean = " ";
 		int i;
-		
+
 		wait.wait(3);
-		
+
 		for (i = 0; i <= 10; i++) {
 			System.out.println(clean);
 		}
 		return clean;
 	}
-	
-	public static void cleanTables (int table[]) {
-		
-		int next;
-		
-		for (next = 0; next < 4; next++) {
-			table[next] = 0;
-		}
-	}
-	
+
 	public static boolean verify(char tableA[], char tableB[]) {
-		
+
 		int next;
 		int contador = 0;
-		
+
 		for (next = 0; next < 4; next++) {
 			if (tableA[next] == tableB[next]) {
 				contador++;
@@ -103,28 +103,18 @@ public class mastermind {
 			return false;
 		}
 	}
+
 	public static void fitxes(char tableA[], char tableB[]) {
 		int next = 0;
-		char color;
-		int a = 0;
-		int b = 0;
-		
-			
+
 		for( next = 0; next < 4; next++) {
 			if(tableB[next] == tableA[next]) {
 				System.out.println("V");
 			}
 			else if(tableB[next] != tableA[next]) {
-					System.out.println("+");
+				System.out.println("+");
 
-				}
+			}
 		}
-		//for( a = 0; a < 4; a++) {
-		//for(b = 0; tableA[a] != tableB[b]; next++) {
-			//if(tableA[a] == tableB[b]) {
-				//System.out.println("X");
-			//}
-		//}
-	//}
 	}
 }
